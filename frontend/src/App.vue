29847b5de6deb8b5196ea501d4624b3776c1b0c4
Joshua Brionne <Joshua.brionne@gmail.com>
2021-04-03 20:41:51 +0200

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -23,6 +9,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ frontend/src/views/waitingRoom.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+      <p> {{ key }}a  </p>
+    <form action="#">
+      <input type="text" v-model="password" @keyup.enter="login"/>
+    </form>
+    <button @click="login">Connexion</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      password: "",
+      key: process.env.VUE_APP_PASS
+    };
+  },
+  methods: {
+    login() {
+      if (this.password == this.key)
+      {
+          this.$router.push({ path: "/accueil" });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
